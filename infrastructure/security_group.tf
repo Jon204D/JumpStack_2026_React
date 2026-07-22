61da69ec12374e4f3baf9e27886b6d9c4c0c989d
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2026-07-21 21:26:03 -0400

    Refactor code structure for improved readability and maintainability

--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -22,6 +22,13 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.student_name}-react-frontend-sg"
   }
